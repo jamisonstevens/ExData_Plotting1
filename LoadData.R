9e344b28c83a1dc579da1f4656855dcd7b251bf0
jamisonstevens <55597535+jamisonstevens@users.noreply.github.com>
2020-06-02 23:42:58 -0400

    Delete LoadData.R
    Delete LoadData()

--- LoadData.R
+++
@@ -1,8 +0,0 @@
-LoadData <- function() {
-  setwd("/Users/student/Documents/Learning/R/ExData_Plotting1")
-  datafile <- paste(getwd(), "/household_power_consumption.txt", sep = "")
-  power_consumption <- read.table(datafile, header = TRUE, sep = ";", na.strings = "?")
-  two_day_consumption <- subset(power_consumption, Date == "1/2/2007" | Date == "2/2/2007")
-
-  return(two_day_consumption)
-}
